fb270ee87230f6e139e2d570cd33d0f22b5a89df
aburtasenkov <aburtasenkov@gmail.com>
2024-12-19 17:47:34 +0100

    separated coordinate and notation implementations and added operator== and operator!= to coordinate

---
+++ chess_coordinate.hpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <assert.h>
+
+const int CHESSCOORDINATE_INVALID_VALUE = -1;
+const char ALPHABET_TO_INT = '`'; // character 1 int smaller than 'a' --> 'a' has index 1
+const int INDEX_TO_NUM = 1;
+
+struct ChessCoordinate{
+    ChessCoordinate ()
+        :character(CHESSCOORDINATE_INVALID_VALUE), integer(CHESSCOORDINATE_INVALID_VALUE) {   }
+
+    ChessCoordinate(int c, int i) 
+    {    
+        assert_data();
+        character = c;
+        integer = i;
+    }
+
+    void assert_data()
+    {
+        assert(0 < character && character < 9); // index for column between 1 and 8 incl.
+        assert(-1 < integer && integer < 8);    // index for row between 0 and 7 incl.
+    }
+
+    // both are integer values, so I can use them for indexing ChessBoard[integer][character]
+    int character;
+    int integer;
+};
+
+std::ostream& operator<<(std::ostream& os, const ChessCoordinate& move)
+{
+    return os << (char)(ALPHABET_TO_INT + move.character) << move.integer + INDEX_TO_NUM;
+}
+
+std::istream& operator>>(std::istream& is, ChessCoordinate& move)
+{
+    char ch;
+    int i;
+
+    is >> ch >> i;
+    assert(is.good());
+
+    move.character = tolower(ch) - ALPHABET_TO_INT;
+    move.integer = i - INDEX_TO_NUM;
+
+    move.assert_data();
+
+    return is;
+} 
+
+bool operator==(const ChessCoordinate& x, const ChessCoordinate& y)
+{
+    return ((x.character == y.character) && (x.integer == y.integer));
+}
+
+bool operator!=(const ChessCoordinate& x, const ChessCoordinate& y)
+{
+    return !operator==(x,y);
+}
--- chess_notation.hpp
+++ chess_notation.hpp
@@ -1,49 +1,18 @@
-#include <iostream>
-#include <assert.h>
+#include "chess_coordinate.hpp"
 
-const int CHESSMOVE_INVALID_VALUE = -1;
-const char ALPHABET_TO_INT = '`'; // character 1 int smaller than 'a' --> 'a' has index 1
-const int INDEX_TO_NUM = 1;
+struct ChessNotation
+{
+    ChessNotation()
+        :x(ChessCoordinate{}), y(ChessCoordinate{}) {   }
 
-struct ChessMove{
-    ChessMove ()
-        :character(CHESSMOVE_INVALID_VALUE), integer(CHESSMOVE_INVALID_VALUE) {   }
-
-    ChessMove(int c, int i) 
-    {    
-        assert_data();
-        character = c;
-        integer = i;
-    }
-
-    void assert_data()
+    ChessNotation(const ChessCoordinate& f, const ChessCoordinate& t)
+        :x(f), y(t) 
     {
-        assert(0 < character && character < 9); // index for column between 1 and 8 incl.
-        assert(-1 < integer && integer < 8);    // index for row between 0 and 7 incl.
+        x.assert_data();
+        y.assert_data();
+        assert(x != y);
     }
 
-    // both are integer values, so I can use them for indexing ChessBoard[integer][character]
-    int character;
-    int integer;
+    ChessCoordinate x;  // move from x
+    ChessCoordinate y;  // to y
 };
-
-std::ostream& operator<<(std::ostream& os, const ChessMove& move)
-{
-    return os << (char)(ALPHABET_TO_INT + move.character) << move.integer + INDEX_TO_NUM;
-}
-
-std::istream& operator>>(std::istream& is, ChessMove& move)
-{
-    char ch;
-    int i;
-
-    is >> ch >> i;
-    assert(is.good());
-
-    move.character = tolower(ch) - ALPHABET_TO_INT;
-    move.integer = i - INDEX_TO_NUM;
-
-    move.assert_data();
-
-    return is;
-} 
