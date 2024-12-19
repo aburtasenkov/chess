26939e9e2201520c30f5a108a6fd4c08dce76d82
aburtasenkov <aburtasenkov@gmail.com>
2024-12-19 13:57:08 +0100

    renamed header

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
-#include "Chess_board.hpp"
+#include "chess_board.hpp"
 using namespace chess_board_namespace;
 
 ChessBoard board;
 
-Board<std::vector, char> b{8,2, 'X'};
-
 int main()
 {
-    std::cout << b << "\n";
-    std::cout << board << "\n";
+    std::cout << board;
     return 0;
 }
 
