2e450fd71071a7295f2aad56dc614e151d14d406
aburtasenkov <aburtasenkov@gmail.com>
2024-12-19 14:08:38 +0100

    Added repeating using statements for better readability in the file

--- chess_board.hpp
+++ chess_board.hpp
@@ -5,6 +5,14 @@ namespace cbn
 {
     #include "chess_board_constants.hpp"
 
+    template <typename T, typename A>
+    using container_type = std::vector<T, A>;
+        
+    template <typename T>
+    using allocator_type = std::allocator<T>;
+        
+    using value_type = std::string;     // std::string because it is not bound to 1 byte of data --> no overflow
+
     class ChessBoard{
 
         public:
