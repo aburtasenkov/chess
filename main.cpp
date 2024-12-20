73119affbcd1be159775581d08e8db2192d2e981
aburtasenkov <aburtasenkov@gmail.com>
2024-12-20 21:26:22 +0100

    Changed main for testing LegalMoves and added Exception handling

--- main.cpp
+++ main.cpp
@@ -4,14 +4,25 @@ using namespace cbn;
 ChessBoard board;
 
 int main()
-{
-    while (true)
-    {
-        std::cout << board;
-        ChessCoordinate x, y;
-        std::cin >> x >> y;
-        board.move({x,y});
-    }
+try {
+    // while (true)
+    // {
+    //     std::cout << board;
+    //     ChessCoordinate x, y;
+    //     std::cin >> x >> y;
+    //     board.move({x,y});
+    // }
+    std::cout << board;
+    ChessCoordinate x;
+    std::cin >> x;
+    Legalmoves lm {board, x};
+    std::vector<ChessCoordinate> legal_moves = lm();
+    std::cout << legal_moves.size() << "\n";
+    std::cout << board;
     return 0;
+}
+catch (Exception& e)
+{
+    std::cerr << e.what() << "\n";
 }
 
