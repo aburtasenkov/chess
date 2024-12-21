b10896ff1e869a6deb31bcc092f65a1736658f2b
aburtasenkov <aburtasenkov@gmail.com>
2024-12-21 20:28:08 +0100

    Made a function for legal moves for pawns and put all in namespace for readability

--- chess_board.hpp
+++ chess_board.hpp
@@ -70,83 +70,6 @@ namespace cbn
 
     };
 
-    class Legalmoves
-    {
-        public:
-            Legalmoves(const ChessBoard& b, const ChessCoordinate& l)
-                :board(b), location(l)   
-                {
-                    piece_info.value = board[location];
-
-                    if (piece_info.value == EMPTY_SQUARE)
-                        throw EmptySquareError;
-
-                    piece_info.type = piece_type_map.at(piece_info.value);
-
-                    // if in piece_color_white_vector --> white, else black
-                    if (std::find(piece_color_white_vector.begin(), piece_color_white_vector.end(), piece_info.value) != piece_color_white_vector.end())
-                        piece_info.color = Piece_color::White;
-                    else 
-                        piece_info.color = Piece_color::Black;
-                }
-
-            const container_type<ChessCoordinate, allocator_type<ChessCoordinate>> operator()()
-            // return a container containing all legal moves for kind located at location
-            {
-                container_type<ChessCoordinate, allocator_type<ChessCoordinate>> legal_moves{};
-
-                ChessCoordinate legal_move = location;  // temporal variable for legal moves
-
-                if (piece_info.type == Piece_type::Pawn)
-                {
-                    if (piece_info.color == Piece_color::White)
-                    {
-                        if (location.integer = 7)
-                        {
-                            legal_move.integer = location.integer - 2;
-                            legal_moves.push_back(legal_move);
-                        }
-                    }
-
-                    else if (piece_info.color == Piece_color::Black)
-                    {
-
-                    }
-                    // check if starting rank for WHITE and BLACK
-                    // if yes return 2 front squares
-                    // return 1 front square
-                    // Check for en passant
-                }
-
-                else if (piece_info.type == Piece_type::Rook)
-                {
-                    // iterate up, down, left, right
-                }
-
-                else if (piece_info.type == Piece_type::Knight)
-                {
-                    // iterate left, right
-                }
-
-                else if (piece_info.type == Piece_type::Bishop)
-                {
-                    // iterate left and right
-                }
-
-                else if (piece_info.type == Piece_type::Queen)
-                {
-                    // make up some smart stuff here, code later
-                }
-
-                return legal_moves;
-            }
-            
-        private:
-            const ChessBoard& board;
-            Piece_data piece_info;
-            ChessCoordinate location;
-    };
-
     // Function and Method Definition
 
     std::ostream& operator<<(std::ostream& os, const ChessBoard& cb)
@@ -158,4 +81,118 @@ namespace cbn
     {
         board = DEFAULT_CHESS_BOARD;
     }
+
+    bool square_is_empty(const value_type& val)
+    {
+        return val == EMPTY_SQUARE;
+    }
 }
+
+namespace lmn
+{
+    using namespace cbn;
+
+    class Legalmoves
+    {
+        public:
+            Legalmoves(const ChessBoard& b)
+                :board(b)
+            {   }
+            // calculate all legal moves for piece at location
+            const container_type<ChessCoordinate, allocator_type<ChessCoordinate>> operator()(const ChessCoordinate& location);
+            
+        private:
+            const ChessBoard& board;
+            Piece_data get_piece_info(const ChessCoordinate& location);
+            void append_legalmoves_pawn(container_type<ChessCoordinate, allocator_type<ChessCoordinate>>& legal_moves, const Piece_data& piece_info, const ChessCoordinate& location);
+    };
+
+    Piece_data Legalmoves::get_piece_info(const ChessCoordinate& location)
+    {
+        Piece_data piece_info;
+
+        // make piece_info data
+        {
+        piece_info.value = board[location];
+
+        if (square_is_empty(board[location]))
+            throw EmptySquareError;
+
+        piece_info.type = piece_type_map.at(piece_info.value);
+
+        // if in piece_color_white_vector --> white, else black
+        if (std::find(piece_color_white_vector.begin(), piece_color_white_vector.end(), piece_info.value) != piece_color_white_vector.end())
+            piece_info.color = Piece_color::White;
+        else 
+            piece_info.color = Piece_color::Black;
+        }
+
+        return piece_info;
+    }
+
+    void Legalmoves::append_legalmoves_pawn(container_type<ChessCoordinate, allocator_type<ChessCoordinate>>& legal_moves, const Piece_data& piece_info, const ChessCoordinate& location)
+    // append legal_moves with possible moves for pawns
+    {
+        // Check for en passant, TODO
+
+        ChessCoordinate front = location - ChessCoordinate{0,1};    // coordinate in front of pawn
+        ChessCoordinate front_2 = front - ChessCoordinate{0,1};     // coordinate 2 squares in front of pawn
+        Piece_color color = Piece_color::White;
+        int starting_row = RANK_7_INDEX;
+
+        // Do once for white, once for black
+        for (int _ = 0; _ < 2; ++_)
+        {
+            if (piece_info.color == color)
+            {
+                if (square_is_empty(board[front]))
+                {
+                    legal_moves.push_back(front);
+                    
+                    if (location.integer == starting_row && square_is_empty(board[front_2]))    // pawns on starting row can move 2 squares
+                        legal_moves.push_back(front_2);
+                }
+                return;
+            }
+
+            color = Piece_color::Black;
+            starting_row = RANK_2_INDEX;
+            front = location + ChessCoordinate{0,1};
+            front_2 = front + ChessCoordinate{0,1};
+        }
+        return;
+    }
+        
+    const container_type<ChessCoordinate, allocator_type<ChessCoordinate>> Legalmoves::operator()(const ChessCoordinate& location)
+    // return a container containing all legal moves for kind located at location
+    {
+        Piece_data piece_info = get_piece_info(location);
+
+        container_type<ChessCoordinate, allocator_type<ChessCoordinate>> legal_moves{};
+
+        if (piece_info.type == Piece_type::Pawn)
+            append_legalmoves_pawn(legal_moves, piece_info, location);
+
+        else if (piece_info.type == Piece_type::Rook)
+        {
+            // iterate up, down, left, right
+        }
+
+        else if (piece_info.type == Piece_type::Knight)
+        {
+            // iterate left, right
+        }
+
+        else if (piece_info.type == Piece_type::Bishop)
+        {
+            // iterate left and right
+        }
+
+        else if (piece_info.type == Piece_type::Queen)
+        {
+            // make up some smart stuff here, code later
+        }
+
+        return legal_moves;
+    }
+};
